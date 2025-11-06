0279c5dfe0a211dffdba9786a44f8ce9cb1b26ca
Azat S. <to@azat.io>
2025-11-07 00:48:28 +0300

    chore: move to ts cspell config

---
+++ cspell.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'cspell'
+
+export default defineConfig({
+  words: [
+    'azat',
+    'changelogen',
+    'changelogithub',
+    'commitish',
+    'crosspost',
+    'gitea',
+    'humanwhocodes',
+    'nanospinner',
+    'oauthtoken',
+    'octocat',
+    'rcompare',
+    'rolldown',
+    'segs',
+    'zipball',
+  ],
+  ignorePaths: [
+    '.github',
+    'changelog.md',
+    'license',
+    'pnpm-lock.yaml',
+    'tsconfig.json',
+  ],
+  dictionaries: ['css', 'html', 'node', 'npm', 'typescript'],
+  useGitignore: true,
+  language: 'en',
+})
